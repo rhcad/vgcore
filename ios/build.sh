249a6817b8c436a4d3fdda72d8d368cb1bef1bc6
Zhang Yungui <rhcad@hotmail.com>
2014-03-20 06:56:21 +0800

    Support xcode5.1 and iphoneos7.1

--- ios/build.sh
+++ ios/build.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 # Type './build.sh' to make iOS libraries.
 
+iphoneos71=`xcodebuild -showsdks | grep -i iphoneos7.1`
 iphoneos70=`xcodebuild -showsdks | grep -i iphoneos7.0`
 iphoneos61=`xcodebuild -showsdks | grep -i iphoneos6.1`
 iphoneos51=`xcodebuild -showsdks | grep -i iphoneos5.1`
 iphoneos43=`xcodebuild -showsdks | grep -i iphoneos4.3`
 
+if [ -n "$iphoneos71" ]; then
+    xcodebuild -project TouchVGCore/TouchVGCore.xcodeproj -sdk iphoneos7.1 -configuration Release
+else
 if [ -n "$iphoneos70" ]; then
     xcodebuild -project TouchVGCore/TouchVGCore.xcodeproj -sdk iphoneos7.0 -configuration Release
 else
@@ -17,6 +21,7 @@ if [ -n "$iphoneos51" ]; then
 else
 if [ -n "$iphoneos43" ]; then
     xcodebuild -project TouchVGCore/TouchVGCore.xcodeproj -sdk iphoneos4.3 -configuration Release
+fi
 fi
 fi
 fi
