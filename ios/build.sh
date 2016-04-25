bd3aaba85d47fe62168ad99ea45c3a37429af799
Zhang Yungui <rhcad@hotmail.com>
2016-04-25 13:55:14 +0800

    Update build.sh

--- ios/build.sh
+++ ios/build.sh
@@ -3,38 +3,9 @@
 # Type './build.sh -arch arm64' to make iOS libraries for iOS 64-bit.
 # Type './build.sh clean' to remove object files.
 
-iphoneos71=`xcodebuild -showsdks | grep -i iphoneos7.1`
-iphoneos70=`xcodebuild -showsdks | grep -i iphoneos7.0`
-iphoneos61=`xcodebuild -showsdks | grep -i iphoneos6.1`
-iphoneos51=`xcodebuild -showsdks | grep -i iphoneos5.1`
-iphoneos43=`xcodebuild -showsdks | grep -i iphoneos4.3`
-
-if [ -n "$iphoneos71" ]; then
-    xcodebuild -project TouchVGCore/TouchVGCore.xcodeproj $1 $2 -sdk iphoneos7.1 -configuration Release
-    xcodebuild -project TouchVGCore/VGShape.xcodeproj $1 $2 -sdk iphoneos7.1 -configuration Release
-else
-if [ -n "$iphoneos70" ]; then
-    xcodebuild -project TouchVGCore/TouchVGCore.xcodeproj $1 $2 -sdk iphoneos7.0 -configuration Release
-    xcodebuild -project TouchVGCore/VGShape.xcodeproj $1 $2 -sdk iphoneos7.0 -configuration Release
-else
-if [ -n "$iphoneos61" ]; then
-    xcodebuild -project TouchVGCore/TouchVGCore.xcodeproj $1 $2 -sdk iphoneos6.1 -configuration Release
-    xcodebuild -project TouchVGCore/VGShape.xcodeproj $1 $2 -sdk iphoneos6.1 -configuration Release
-else
-if [ -n "$iphoneos51" ]; then
-    xcodebuild -project TouchVGCore/TouchVGCore.xcodeproj $1 $2 -sdk iphoneos5.1 -configuration Release
-    xcodebuild -project TouchVGCore/VGShape.xcodeproj $1 $2 -sdk iphoneos5.1 -configuration Release
-else
-if [ -n "$iphoneos43" ]; then
-    xcodebuild -project TouchVGCore/TouchVGCore.xcodeproj $1 $2 -sdk iphoneos4.3 -configuration Release
-    xcodebuild -project TouchVGCore/VGShape.xcodeproj $1 $2 -sdk iphoneos4.3 -configuration Release
-fi
-fi
-fi
-fi
-fi
+xcodebuild -project TouchVGCore/TouchVGCore.xcodeproj $1 $2 -configuration Release -target TouchVGCore
 
 mkdir -p output/TouchVGCore
 cp -R TouchVGCore/build/Release-universal/*.a output
 cp -R TouchVGCore/build/Release-universal/include/TouchVGCore output
-cp -R TouchVGCore/build/Release-universal/include/VGShape output
+#cp -R TouchVGCore/build/Release-universal/include/VGShape output
